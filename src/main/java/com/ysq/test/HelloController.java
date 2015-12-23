4390dec5218d1a5ce15e0726e4715c6d2a067668
yangshengqing <340711340@qq.com>
2015-12-23 19:56:56 +0800

    可以在jsp中取对象的属性和调用对象的方法

--- src/main/java/com/ysq/test/HelloController.java
+++ src/main/java/com/ysq/test/HelloController.java
@@ -4,14 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ysq.test.model.User;
+
 @Controller
 public class HelloController {
 	@RequestMapping("/hello")
 	public ModelAndView hello() {
 		ModelAndView mv = new ModelAndView();
+		User user = new User();
+		user.setName("name1");
+		user.setPassword("password1");
 		mv.addObject("spring", "spring mvc");
+		mv.addObject("user", user);
 		mv.setViewName("hello");
 		return mv;
-
+	}
+	@RequestMapping("/hello1")
+	public String hello1() {
+		return "你好啊";
 	}
 }
--- src/main/webapp/WEB-INF/views/hello.jsp
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -1,14 +1,12 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>userInfo</title>
+    <meta charset="UTF-8">
+    <title>Title</title>
 </head>
 <body>
-
-     姓名： ${spring}
-
+	你好${user.getName() }
 </body>
 </html>
