7fa5fdbd25fd8afd56b766841632ff907fbd8a77
yangshengqing <340711340@qq.com>
2016-09-23 10:49:40 +0800

    添加文件上传功能、添加requestbody测试功能

--- src/main/java/com/ysq/test/controller/AccessController.java
+++ src/main/java/com/ysq/test/controller/AccessController.java
@@ -1,9 +1,20 @@
 package com.ysq.test.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.ysq.test.entity.User;
 
 @Controller
 @RequestMapping("/access")
@@ -15,6 +26,30 @@ public class AccessController {
 		mv.addObject("message", "test success");
 		mv.setViewName("hello");
 		return mv;
+	}
+
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	@ResponseBody
+	public Object saveEssays(HttpServletRequest request, HttpServletResponse response, String token) throws Exception {
+		MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+		List<MultipartFile> files = multipartRequest.getFiles("file");
+		return "{'a': '1'}";
+	}
+	
+	@RequestMapping(value = "/testRequestBody", method = RequestMethod.POST)
+	@ResponseBody
+	public Object testRequestBody(HttpServletRequest request, HttpServletResponse response,@RequestBody User user) throws Exception {
+		@SuppressWarnings("unused")
+		String token1 = user.getName();
+		return user;
+	}
+	
+	@RequestMapping(value = "/testParams", method = RequestMethod.POST)
+	@ResponseBody
+	public Object testParams(HttpServletRequest request, HttpServletResponse response, String token) throws Exception {
+		@SuppressWarnings("unused")
+		String token1 = token;
+		return token1;
 	}
 
 }
