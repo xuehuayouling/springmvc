8cfa9bea37ab9da511c099fa249e356c80355a60
yangshengqing <340711340@qq.com>
2016-10-24 15:07:50 +0800

    修改表名

--- src/main/java/com/ysq/test/entity/User.java
+++ src/main/java/com/ysq/test/entity/User.java
@@ -6,10 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
-@Entity
-@Table(name = "user")
+@Entity(name = "t_user")
 public class User {
 
 	private int id;
